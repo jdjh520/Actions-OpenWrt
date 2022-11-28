a46435091b4d9d55d3c3048f3c89fa0954142e88
jdjh520 <57866086+jdjh520@users.noreply.github.com>
2022-11-28 15:08:28 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# 默认开启wifi
+# sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# sed -i 's/wireless.${name}.disabled=1/wireless.${name}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改内核版本
+
+# sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
+
+#修改主题
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
